8d57823780bba7eae0e2d318aafb621d8d096e04
Santoro Fabrizio <fabrizio1996@live.fr>
2019-10-20 10:58:50 +0200

    Starting moving ai to jobs

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -10,6 +10,7 @@ public class AI : MonoBehaviour
     
 
     private bool isEntering;
+    private bool isLeaving;
 
     private int dirtyness;
 
@@ -41,6 +42,7 @@ public class AI : MonoBehaviour
     private void Start()
     {
         StartCoroutine(FadeIn());
+        StartCoroutine(LookForADrink());
     }
 
     IEnumerator FadeIn()
@@ -55,19 +57,30 @@ public class AI : MonoBehaviour
         }
     }
 
+    IEnumerator FadeOut()
+    {
+        for (int i = 10; i > 0; i--)
+        {
+            Color color = spRenderer.color;
+            color.a = i * 0.1f;
+
+            spRenderer.color = color;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-       
         if (isEntering)
         {
             transform.position =
-                Vector3.MoveTowards(transform.position, AiManager.instance.danceFloorPosition.position, Time.deltaTime);
+                Vector3.MoveTowards(transform.position, AiManager.instance.danceFloorPosition.position, 0.05f);
 
             if ((transform.position - AiManager.instance.danceFloorPosition.position).magnitude < 0.5f)
                 isEntering = false;
         }
-        else if (drinkyness < 0 || dirtyness < 0)
+        else if (isLeaving)
         {
             transform.position =
                 Vector3.MoveTowards(transform.position, AiManager.instance.spawnPoint.position, Time.deltaTime);
@@ -83,11 +96,27 @@ public class AI : MonoBehaviour
     {
         //go to the bar, checks if there is enough drink
         //if not drinkyness--;
-        yield return null;
+        while (drinkyness != 0)
+        {
+            yield return new WaitForSeconds(AiManager.instance.timeBeforeLookForDrink);
+            
+
+            while ((transform.position - AiManager.instance.barPosition.position).magnitude > 0.5f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, AiManager.instance.barPosition.position,
+                    0.05f);
+                yield return new WaitForEndOfFrame();
+            }
+            
+            //check for drink availability
+        }
+
+        isLeaving = true;
     }
 
     IEnumerator CheckTheFloor()
     {
+        //checks the floor state
         yield return null;
     }
 }
--- Assets/Scripts/AI/AiManager.cs
+++ Assets/Scripts/AI/AiManager.cs
@@ -9,13 +9,18 @@ public class AiManager : MonoBehaviour
     
     public int startingDrinkness;
     public int startingDirtyness;
+
+    public float timeBeforeLookForDrink;
+    public float timeBeforeLookForDirty;
     
     public AI prefabAI;
 
     public List<AI> ais;
     public Transform spawnPoint;
     public Transform danceFloorPosition;
-    
+
+    public Transform barPosition;
+
     private void Awake()
     {
         if (!instance)
