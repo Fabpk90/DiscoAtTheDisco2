3a35800806bbaeb95fb07e7d102decb13cfd1d6b
Pamplemousse <valentin-didomenico@orange.fr>
2019-10-21 12:31:32 +0200

    Update Sound Integration

--- Assets/Mood_State.cs
+++ Assets/Mood_State.cs
@@ -10,7 +10,7 @@ public class Mood_State : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AkSoundEngine.SetState("Mood", "Mood_Level_00");
+        //AkSoundEngine.SetState("Mood", "Mood_Level_00");
     }
 
     // Update is called once per frame
